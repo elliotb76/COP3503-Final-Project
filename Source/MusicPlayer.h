31dd7658ff2db0a970105681d08d9364ff2d3e58
elliotb76 <elliotb76@gmail.com>
2016-12-05 00:46:36 -0500

    Update MusicPlayer.h

--- Source/MusicPlayer.h
+++ Source/MusicPlayer.h
@@ -1,1 +1,26 @@
 #pragma once
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<Windows.h>
+#include <Mmsystem.h>
+#include <mciapi.h>
+using namespace std;
+#pragma comment(lib, "Winmm.lib")
+
+
+class MusicPlayer{
+	private string filePath;
+	private bool isOpen;
+	private bool isPlaying;
+	
+	public:
+		MusicPlayer();
+		void Play(string filePath);
+		void Pause(string filePath);
+		void Resume(string filePath);
+		void Stop(string filePath);
+		std::wstring s2ws(const std::string& s);
+};
+
+
